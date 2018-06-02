156a3ce03fd7a8a61f03063f58a7f117e235810e
Joel Adriance <adriancj@oregonstate.edu>
2018-06-02 09:51:27 -0700

    Added recursive node functions and sort functions

--- City.cpp
+++ City.cpp
@@ -10,6 +10,8 @@
 using std::string;
 using std::to_string;
 
+City::CompareMethod City::compareBy = CMP_ID;
+
 City::City(int id, int x, int y) {
 	this->id = id;
 	this->xCoord = x;
@@ -20,3 +22,16 @@ string City::ToString() {
 	return "City " + to_string(id) + ": (" + to_string(xCoord) + ", " + to_string(yCoord) + ")";
 	
 };
+
+bool City::operator < (const City& other) const {
+	if (this->compareBy == CMP_X)
+		return this->xCoord < other.xCoord;
+	else if (this->compareBy == CMP_Y)
+		return this->yCoord < other.yCoord;
+	else
+		return this->id < other.id;
+}
+
+bool City::Compare(const City* lhs, const City* rhs) {
+	return *lhs < *rhs;
+}
--- SpatialTree.cpp
+++ SpatialTree.cpp
@@ -6,16 +6,39 @@
  ****************************************/
 #include <vector>
 #include <queue>
+#include <algorithm>
+
 #include "SpatialTree.h"
 
 using std::vector;
 using std::queue;
+using std::sort;
 
-SpatialTree::SpatialTree(vector<City*> cities) {
+SpatialTree::SpatialTree() {
+	size = 0;
 	root = NULL;
+}
+// Build a tree from a list of cities.
+// Cities are sorted and split by alternating x/y
+SpatialTree::SpatialTree(vector<City*> cities) {
+	size = cities.size();
+	
+	// Make a copy of the cities list for sorting in place
+	/*if size < 1 {
+		root = NULL;
+	} else {
+		
+	}*/
+	;
 };
 
+bool SpatialTree::AllVisited() const {
+	return root && root->AllVisited();
+}
+
 void SpatialTree::ClearVisited() {
+	if (root)
+		root->ClearVisited();
 };
 
 City* SpatialTree::GetNextNearest(City* city) {
@@ -25,3 +48,7 @@ City* SpatialTree::GetNextNearest(City* city) {
 queue<City*> SpatialTree::GetKNearest(City* city, int k) {
 	return queue<City*>();
 };
+
+const int SpatialTree::Count() {
+	return size;
+}

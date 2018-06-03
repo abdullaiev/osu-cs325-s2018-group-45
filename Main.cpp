65cff90fdfbd6557a807217cf8a77f341c686c6a
illia-osu <abdullai@oregonstate.edu>
2018-06-02 21:34:43 -0700

    Remove commented out code.

--- Main.cpp
+++ Main.cpp
@@ -21,35 +21,6 @@ using std::cin;
 
 
 int main(int argc, char *argv[]) {
-/**
-	vector<City*> cities;
-	
-	cities.push_back(new City(5, 5, 1));
-	cities.push_back(new City(4, 3, 3));
-	cities.push_back(new City(3, 1, 2));
-	cities.push_back(new City(2, 2, 5));
-	cities.push_back(new City(1, 4, 4));
-	
-	sort(cities.begin(), cities.end(), City::CompareID);
-	for (int i = 0; i < cities.size(); ++i) {
-		cout << cities[i]->ToString() << endl;
-	}
-	cout << endl;
-	
-	sort(cities.begin(), cities.end(), City::CompareX);
-	for (int i = 0; i < cities.size(); ++i) {
-		cout << cities[i]->ToString() << endl;
-	}
-	cout << endl;
-	
-		sort(cities.begin(), cities.end(), City::CompareY);
-	for (int i = 0; i < cities.size(); ++i) {
-		cout << cities[i]->ToString() << endl;
-	}
-	cout << endl;
-
-**/
-
     string inputFileName;
     string outputFileName;
 
